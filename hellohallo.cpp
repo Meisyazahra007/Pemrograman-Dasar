728d913037d4c26fa46fb67f38acfc8458863df4
Meisyazahra007 <111963561+Meisyazahra007@users.noreply.github.com>
2022-10-08 16:27:57 +0700

    Add files via upload

---
+++ hellohallo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama: Meisya Zahra" <<endl;
+    cout << "Kelas: XPPLG1" <<endl;
+    cout << "===============" <<endl;
+    cout << "Angka1 = 10" <<endl;
+    cout << "Angka2 = 5" <<endl;
+    cout << "Hasil:"<<10+5 <<endl;
+    
+    return 0; 
+}
