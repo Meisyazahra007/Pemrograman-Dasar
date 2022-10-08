b6294be3b7dd9330240e0a8d6baf217da753db1c
Meisyazahra007 <111963561+Meisyazahra007@users.noreply.github.com>
2022-10-08 16:50:11 +0700

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int angka []= {1,3,5,7,9};
+    cout <<"angka ="<<angka[2]<<endl;
+    char huruf []= {'A','I','U','E','O'};
+    cout << "huruf ="<<huruf[4]<<endl;
+    string nama []= {"Algis","Osep","Amel"};
+    cout << "Nama ="<<nama[0]<<endl;
+    int nilai[3][4] = {
+      {80,85},
+      {90,92,94,96},
+      {75,78,79}
+ };
+    cout << "nilai ="<<nilai[2][1]<<endl;
+    }
