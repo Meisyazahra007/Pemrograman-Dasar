c35eb335fff43d8b0643db92fbd9236c415ac660
Meisyazahra007 <111963561+Meisyazahra007@users.noreply.github.com>
2022-10-08 16:36:49 +0700

    Add files via upload

---
+++ tambah.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout <<"Aplikasi penjumlahan"<<endl;
+    cout << "Masukan angka pertama=";
+    int angka1;
+    cin >> angka1;
+    cout <<"masukan angka kedua=";
+    int angka2;
+    cin >> angka2;
+    int hasil;
+    hasil= angka1 + angka2;
+    cout <<"hasil penjumlahan="<<hasil;
+    
+}
