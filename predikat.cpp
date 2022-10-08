79f084fcf4830a12f71ac109eafdcacd6380f7c1
Meisyazahra007 <111963561+Meisyazahra007@users.noreply.github.com>
2022-10-08 16:37:51 +0700

    Add files via upload

---
+++ predikat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int nilai;
+    cout <<"masukan nilai"; cin >>nilai;
+    if (nilai >78){
+      cout <<"Lulus";
+    }else if (nilai == 78){
+      cout <<"kkm";
+    }else{
+      cout <<"tidak Lulus";
+    }
+    
+}
