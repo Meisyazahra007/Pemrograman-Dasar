d44d4ddfdd3865a30bd251ebd9b547a974fca534
Meisyazahra007 <111963561+Meisyazahra007@users.noreply.github.com>
2022-10-08 16:37:12 +0700

    Add files via upload

---
+++ kali.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout <<"Aplikasi perkalian"<<endl;
+    cout << "Masukan angka pertama=";
+    int angka1;
+    cin >> angka1;
+    cout <<"masukan angka kedua=";
+    int angka2;
+    cin >> angka2;
+    int hasil;
+    hasil= angka1 + angka2;
+    cout <<"hasil perkalian="<<hasil;
+}
